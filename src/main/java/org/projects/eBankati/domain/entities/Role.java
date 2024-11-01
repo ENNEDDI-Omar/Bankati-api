631970fc5fa9707db60985f0fbe740a0b7f975ae
ENNEDDI-Omar <enneddiomar@gmail.com>
2024-11-01 11:29:20 +0100

    Mise en place et migration des entites User et Role.

--- src/main/java/org/projects/eBankati/domain/entities/Role.java
+++ src/main/java/org/projects/eBankati/domain/entities/Role.java
@@ -1,4 +1,25 @@
 package org.projects.eBankati.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
 }
--- src/main/java/org/projects/eBankati/domain/entities/User.java
+++ src/main/java/org/projects/eBankati/domain/entities/User.java
@@ -1,4 +1,37 @@
 package org.projects.eBankati.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    private Integer age;
+
+    @Column(name = "monthly_income")
+    private Double monthlyIncome;
+
+    @Column(name = "credit_score")
+    private Integer creditScore;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+
 }
