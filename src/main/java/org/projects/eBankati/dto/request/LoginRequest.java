6ade4d7ad20b1b232d7a347976f641e2eed2d02f
ENNEDDI-Omar <enneddiomar@gmail.com>
2024-12-04 13:45:27 +0100

    Configuration des environnements

--- src/main/java/org/projects/eBankati/dto/request/LoginRequest.java
+++ src/main/java/org/projects/eBankati/dto/request/LoginRequest.java
@@ -1,9 +1,15 @@
 package org.projects.eBankati.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
-public class LoginRequest {
+public class LoginRequest
+{
+    @NotBlank(message = "Votre Nom d'utilisateur est obligatoire pour se connecter")
     private String username;
+
+    @NotNull(message = "Votre mot de passe est obligatoire pour se connecter")
     private String password;
 }
